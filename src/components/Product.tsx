904b3ed70c973672a56b488743a20a436e8a0073
Hrithik <slumdog4321@gmail.com>
2024-03-23 23:33:28 +0530

    Fix: Animation Flickering Effect

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -38,33 +38,35 @@ const Product = () => {
       } `}
       ref={ref}
     >
-      <div className="wrapper min-h-[70%] ">
-        <img
-          src="/assets/neopop-left-2.png"
-          alt="mockup"
-          className="absolute left-1/2 bottom-0 translate-x-[-160%] translate-y-[25%] z-[98]"
-        />
-        <img
-          src="/assets/neopop-left-1.png"
-          alt="mockup"
-          className="absolute left-1/2 bottom-0 translate-x-[-105%] translate-y-[15%] z-[99]"
-        />
-        <img
-          src="/assets/neopop-center.png"
-          alt="mockup"
-          className="absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-[5%] z-[100]"
-        />
-        <img
-          src="/assets/neopop-right-1.png"
-          alt="mockup"
-          className="absolute left-1/2 bottom-0 translate-x-[5%] translate-y-[15%] z-[99]"
-        />
-        <img
-          src="/assets/neopop-right-2.png"
-          alt="mockup"
-          className="absolute left-1/2 bottom-0 translate-x-[60%] translate-y-[25%] z-[98]"
-        />
-      </div>
+      {showanimation && (
+        <div className="wrapper min-h-[70%] ">
+          <img
+            src="/assets/neopop-left-2.png"
+            alt="mockup"
+            className="absolute left-1/2 bottom-0 translate-x-[-160%] translate-y-[25%] z-[98]"
+          />
+          <img
+            src="/assets/neopop-left-1.png"
+            alt="mockup"
+            className="absolute left-1/2 bottom-0 translate-x-[-105%] translate-y-[15%] z-[99]"
+          />
+          <img
+            src="/assets/neopop-center.png"
+            alt="mockup"
+            className="absolute left-1/2 bottom-0 translate-x-[-50%] translate-y-[5%] z-[100]"
+          />
+          <img
+            src="/assets/neopop-right-1.png"
+            alt="mockup"
+            className="absolute left-1/2 bottom-0 translate-x-[5%] translate-y-[15%] z-[99]"
+          />
+          <img
+            src="/assets/neopop-right-2.png"
+            alt="mockup"
+            className="absolute left-1/2 bottom-0 translate-x-[60%] translate-y-[25%] z-[98]"
+          />
+        </div>
+      )}
     </div>
   );
 };
