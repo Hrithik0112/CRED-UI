d134ada49ecd50d74fff1841f50932bdd2a03007
Hrithik <slumdog4321@gmail.com>
2024-03-24 20:08:56 +0530

    Feat: Review Caroousel added in Rating section

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,3 +1,34 @@
+import { Carousel, CarouselItem } from "./ReviewCarousel";
+
+const items: CarouselItem[] = [
+  {
+    id: 1,
+    review: `"great app, paying dues of credit card was never that much easy and fast. few more rewarding features also available there like showing credit score, can easily pay rent, getting cashback whenever paying cc dues. lots more. i really loves cred."`,
+    author: "ranesh bhattacharya",
+  },
+  {
+    id: 2,
+    review: `"in love! such a seamless user experience, smooth as freshly waxed pair of legs! very intuitive and easy to use application. good job people, good job."`,
+
+    author: "parul mehra",
+  },
+  {
+    id: 3,
+    review: `"have been using the app for an year... very happy with this. it greatly avoids the need for searching for statements individually and logging in to pay them all. with cred i can happily pay all my cc bills with a single click. love the experience."`,
+    author: "pushpalatha a",
+  },
+  {
+    id: 4,
+    review: `"great app, paying dues of credit card was never that much easy and fast. few more rewarding features also available there like showing credit score, can easily pay rent, getting cashback whenever paying cc dues. lots more. i really loves cred."`,
+    author: "sanjeev kataria",
+  },
+  {
+    id: 5,
+    review: `"i would say one of the best platforms to manage your credit card payments with lot of advantages and rewards. you can even pay rent to your owner using credit card. that's really cool. thanks for all the awesomeness. keep inventing !!"`,
+    author: "jagadeeswar reddy singam",
+  },
+];
+
 const Rating = () => {
   return (
     <div className="flex bg-[url(https://web-images.credcdn.in/_next/assets/images/home-page/hero-bg.png)] bg-cover bg-right-top  relative z-20">
@@ -60,6 +91,9 @@ const Rating = () => {
           </div>
         </div>
         {/* review carousal */}
+        <div className="flex overflow-hidden mt-[60px] mx-auto h-[200px] w-[900px] ">
+          <Carousel items={items} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ReviewCarousel.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+export interface CarouselItem {
+  id: number;
+  review: string;
+  author: string;
+}
+
+export const Carousel: React.FC<{ items: CarouselItem[] }> = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const goToNext = () => {
+    setActiveIndex((prevIndex) => (prevIndex === items.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(goToNext, 5000); // Change slide every 3 seconds
+    return () => clearInterval(interval);
+  }, [activeIndex]);
+
+  return (
+    <div className="relative">
+      <div className="overflow-hidden relative">
+        <div
+          className="flex transition-transform duration-300 ease-in-out"
+          style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+        >
+          {items.map((item) => (
+            <div key={item.id} className="w-full flex-shrink-0 p-4 font-normal">
+              <p className="text-lg pb-8 text-center">{item.review}</p>
+              <p className="text-lg text-center">{item.author}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 z-10 flex space-x-2">
+        {items.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setActiveIndex(index)}
+            className={`h-2 w-2 rounded-full ${
+              index === activeIndex ? "bg-gray-800" : "bg-gray-400"
+            }`}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
